aebad1c620fc0c3c7aacbe6f987ee66d0e6cb816
azizbekhafizov <azizbekhafizov678@gmail.com>
2025-03-19 14:50:27 +0500

    new
    new

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ window.addEventListener('load', function() {
     }, 3000); 
 });
 
-
-
+let body = document.body
+function changeMode() {
+    if(body.classList.contains('dark')) {
+        body.classList.remove('dark');
+    }
+    else {
+        body.classList.add('dark');
+    }
+}
 
   
